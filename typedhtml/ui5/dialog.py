780cd873ce79722869d10efc8f02275bdd2da527
g5becks <techstar.dev@hotmail.com>
2023-07-23 23:10:55 -0400

    feat(ui5): add diaload
    add ui5 dialod and file uploader

---
+++ typedhtml/ui5/dialog.py
@@ -0,0 +1,24 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from typing import Literal, Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class dialog_props(ui5_tag_props, total=False):
+    header_text: str
+    draggable: bool
+    resizable: bool
+    state: Literal["None", "Information", "Success", "Warning", "Error"]
+    stretch: bool
+
+
+class dialog(ui5_tag):
+    """The Dialog component is used to temporarily display some information
+    in a simple popup."""
+
+    def __init__(self, **kwargs: Unpack[dialog_props]) -> None:
+        self.tagname = "ui5-dialog"
+        super().__init__(**kwargs)
---
+++ typedhtml/ui5/file_uploader.py
@@ -0,0 +1,30 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+
+from typing import Literal, Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class file_uploader_props(ui5_tag_props, total=False):
+    accept: str
+    disabled: bool
+    hide_input: bool
+    multiple: bool
+    name: str
+    placeholder: str
+    value: str
+    value_state: Literal["None", "Success", "Warning", "Error"]
+
+
+class file_uploader(ui5_tag):
+    """The ui5-file-uploader opens a file explorer dialog and enables users to upload
+    files. The component consists of input field, but you can provide an HTML element
+    by your choice to trigger the file upload, by using the default slot. Furthermore,
+    you can set the property "hideInput" to "true" to hide the input field."""
+
+    def __init__(self, **kwargs: Unpack[file_uploader_props]):
+        self.tagname = "ui5-file-uploader"
+        super().__init__(**kwargs)
