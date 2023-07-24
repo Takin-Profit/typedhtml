cd3bc8c19d676f1c42877fafdd6d9b0ef87e6855
g5becks <techstar.dev@hotmail.com>
2023-07-23 23:28:17 -0400

    feat(ui5): add icon
    add icon and input elements

---
+++ typedhtml/ui5/icon.py
@@ -0,0 +1,35 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from typing import Literal, Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class icon_props(ui5_tag_props, total=False):
+    name: str
+    accessible_name: str
+    accessible_role: str
+    design: Literal[
+        "Contrast",
+        "Critical",
+        "Default",
+        "Negative",
+        "Neutral",
+        "Positive",
+        "Information",
+        "NonInteractive",
+    ]
+    interactive: bool
+    show_tooltip: bool
+
+
+class icon(ui5_tag):
+    """The ui5-icon component represents an SVG icon. There are two main scenarios how
+    the ui5-icon component is used: as a purely decorative element,
+    or as an interactive element that can be focused and clicked."""
+
+    def __init__(self, **kwargs: Unpack[icon_props]) -> None:
+        self.tagname = "ui5-icon"
+        super().__init__(**kwargs)
---
+++ typedhtml/ui5/input.py
@@ -0,0 +1,36 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from typing import Literal, Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class ui5_input_props(ui5_tag_props):
+    show_clear_icon: bool
+    value: str
+    accessible_name: str
+    accessible_name_ref: str
+    disabled: bool
+    max_length: int
+    name: str
+    no_typeahead: bool
+    placeholder: str
+    preview_item: str
+    readonly: bool
+    required: bool
+    show_suggestions: bool
+    type: Literal["Text", "Number", "Password", "Tel", "Email", "Url"]
+    value_state: Literal["None", "Success", "Warning", "Error", "Information"]
+
+
+class input(ui5_tag):
+    """The ui5-input component allows the user to enter and edit text or numeric values
+        in one line.
+    Additionally, you can provide suggestionItems,
+    that are displayed in a popover right under the input."""
+
+    def __init__(self, **kwargs: Unpack[ui5_input_props]) -> None:
+        self.tagname = "ui5-input"
+        super().__init__(**kwargs)
