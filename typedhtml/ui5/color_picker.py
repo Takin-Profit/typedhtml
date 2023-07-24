cc3543a2bfe780ab80140170d2fce369cf9cc43c
g5becks <techstar.dev@hotmail.com>
2023-07-23 21:52:47 -0400

    feat(ui5): add combobox
    add ui5 combobox

---
+++ typedhtml/ui5/color_picker.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from typing import Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class color_picker_props(ui5_tag_props, total=False):
+    color: str
+
+
+class color_picker(ui5_tag):
+    """The ui5-color-picker allows users to choose any color and provides different
+    input options for selecting colors."""
+
+    def __init__(self, **kwargs: Unpack[color_picker_props]):
+        self.tagname = "ui5-color-picker"
+        super().__init__(**kwargs)
+        super().__init__(**kwargs)
---
+++ typedhtml/ui5/combobox.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Takin Profit. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from typing import Literal, Unpack
+
+from typedhtml.ui5.base import ui5_tag, ui5_tag_props
+
+
+class combobox_props(ui5_tag_props):
+    placeholder: str
+    accessible_name: str
+    accessible_name_ref: str
+    disabled: bool
+    filter: Literal["StartsWithPerTerm", "StartsWith", "Contains", "None"]
+    loading: bool
+    readonly: bool
+    required: bool
+    value: str
+    value_state: Literal["Warning", "Error", "Success", "Information", "None"]
+
+
+class combobox(ui5_tag):
+    """The ui5-combobox component represents a drop-down menu with a list of the
+    available options and a text input field to narrow down the options.
+
+    It is commonly used to enable users to select an option from a predefined list."""
+
+    def __init__(self, **kwargs: Unpack[combobox_props]):
+        self.tagname = "ui5-combobox"
+        super().__init__(**kwargs)
