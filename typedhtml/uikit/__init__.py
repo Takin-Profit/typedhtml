8b42bc9e770c46e79e7240be38c1ce3d516d3bbd
g5becks <techstar.dev@hotmail.com>
2023-07-29 19:26:29 -0400

    feat(uikit): add background classes

--- typedhtml/uikit/__init__.py
+++ typedhtml/uikit/__init__.py
@@ -6,7 +6,122 @@ from .accordion import accordion, accordion_content, accordion_item, accordion_t
 from .alert import alert
 from .article import article, article_title
 
+align_left = "uk-align-left"
+align_right = "uk-align-right"
+align_center = "uk-align-center"
+animation_fade = "uk-animation-fade"
+animation_scale_up = "uk-animation-scale-up"
+animation_scale_down = "uk-animation-scale-down"
+animation_slide_top = "uk-animation-slide-top"
+animation_slide_bottom = "uk-animation-slide-bottom"
+animation_slide_left = "uk-animation-slide-left"
+animation_slide_right = "uk-animation-slide-right"
+animation_slide_top_small = "uk-animation-slide-top-small"
+animation_slide_bottom_small = "uk-animation-slide-bottom-small"
+animation_slide_left_small = "uk-animation-slide-left-small"
+animation_slide_right_small = "uk-animation-slide-right-small"
+animation_kenburns = "uk-animation-kenburns"
+animation_shake = "uk-animation-shake"
+animation_stroke = "uk-animation-stroke"
+animation_reverse = "uk-animation-reverse"
+animation_fast = "uk-animation-fast"
+
+background_default = "uk-background-default"
+background_muted = "uk-background-muted"
+background_primary = "uk-background-primary"
+background_secondary = "uk-background-secondary"
+background_cover = "uk-background-cover"
+background_contain = "uk-background-contain"
+background_width_1_1 = "uk-background-width-1-1"
+background_height_1_1 = "uk-background-height-1-1"
+background_top_left = "uk-background-top-left"
+_background_top_right = "uk-background-top-right"
+background_top_center = "uk-background-top-center"
+background_center_left = "uk-background-center-left"
+background_center_center = "uk-background-center-center"
+background_center_right = "uk-background-center-right"
+background_bottom_left = "uk-background-bottom-left"
+background_bottom_center = "uk-background-bottom-center"
+background_bottom_right = "uk-background-bottom-right"
+background_no_repeat = "uk-background-norepeat"
+background_image_s = "uk-background-image@s"
+background_image_m = "uk-background-image@m"
+background_image_l = "uk-background-image@l"
+background_image_xl = "uk-background-image@xl"
+background_blend_multiply = "uk-background-blend-multiply"
+background_blend_screen = "uk-background-blend-screen"
+background_blend_overlay = "uk-background-blend-overlay"
+background_blend_darken = "uk-background-blend-darken"
+background_blend_lighten = "uk-background-blend-lighten"
+background_blend_color_dodge = "uk-background-blend-color-dodge"
+background_blend_color_burn = "uk-background-blend-color-burn"
+background_blend_hard_light = "uk-background-blend-hard-light"
+background_blend_soft_light = "uk-background-blend-soft-light"
+background_blend_difference = "uk-background-blend-difference"
+background_blend_exclusion = "uk-background-blend-exclusion"
+background_blend_hue = "uk-background-blend-hue"
+background_blend_saturation = "uk-background-blend-saturation"
+background_blend_color = "uk-background-blend-color"
+background_blend_luminosity = "uk-background-blend-luminosity"
+
+
 __all__ = [
+    "background_blend_multiply",
+    "background_blend_screen",
+    "background_blend_overlay",
+    "background_blend_darken",
+    "background_blend_lighten",
+    "background_blend_color_dodge",
+    "background_blend_color_burn",
+    "background_blend_hard_light",
+    "background_blend_soft_light",
+    "background_blend_difference",
+    "background_blend_exclusion",
+    "background_blend_hue",
+    "background_blend_saturation",
+    "background_blend_color",
+    "background_blend_luminosity",
+    "background_image_xl",
+    "background_image_l",
+    "background_image_m",
+    "background_image_s",
+    "background_no_repeat",
+    "background_bottom_right",
+    "background_bottom_center",
+    "background_bottom_left",
+    "background_center_right",
+    "background_center_center",
+    "background_center_left",
+    "background_top_center",
+    "_background_top_right",
+    "background_top_left",
+    "background_height_1_1",
+    "background_width_1_1",
+    "background_contain",
+    "background_cover",
+    "background_secondary",
+    "background_primary",
+    "background_muted",
+    "background_default",
+    "animation_fast",
+    "animation_reverse",
+    "animation_stroke",
+    "animation_shake",
+    "animation_kenburns",
+    "animation_slide_right_small",
+    "animation_slide_left_small",
+    "animation_slide_bottom_small",
+    "animation_slide_top_small",
+    "animation_scale_up",
+    "animation_scale_down",
+    "animation_slide_top",
+    "animation_slide_bottom",
+    "animation_slide_left",
+    "animation_fade",
+    "animation_slide_right",
+    "align_center",
+    "align_right",
+    "align_left",
     "article_title",
     "article",
     "alert",
